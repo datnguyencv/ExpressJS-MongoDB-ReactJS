dc4d1ed8780bd520bf863ec935bb556c705b3f23
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-11-16 17:24:05 +0700

    update fix url api

--- src/components/Until.jsx
+++ src/components/Until.jsx
@@ -1,3 +1,6 @@
-const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const apiUrl = 
+'https://expressjs-mongodb-api.testwebs1.repl.co'
+// import.meta.env.VITE_API_BASE_URL 
+|| 'http://localhost:3000';
 
 export default apiUrl;
